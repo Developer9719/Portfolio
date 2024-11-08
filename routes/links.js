c7c145a3877240d0d80b09cac906dd8939ceb9d8
Justin Battrick <justin.bat19@hotmail.com>
2024-11-08 13:24:54 -0500

    Add page title to the body class for CSS targeting

--- routes/links.js
+++ routes/links.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('links', { 
-    title: 'Justin Battrick' 
+    title: 'Links' 
   });
 });
 
